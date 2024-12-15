61354f2ff0a32465884970d47d379c780b06fc70
Pratik Karande <72016219+CyberPratik@users.noreply.github.com>
2024-12-15 16:00:52 +0530

    Add files via upload

---
+++ sudoku_solver.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to check if a number can be placed at a given position
+bool isValid(const vector<vector<int>>& board, int row, int col, int num) {
+    // Check the row
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == num) {
+            return false;
+        }
+    }
+
+    // Check the column
+    for (int i = 0; i < 9; i++) {
+        if (board[i][col] == num) {
+            return false;
+        }
+    }
+
+    // Check the 3x3 box
+    int boxRow = row - row % 3;
+    int boxCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[boxRow + i][boxCol + j] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+// Function to solve the Sudoku puzzle using backtracking
+bool solveSudoku(vector<vector<int>>& board) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] == 0) {
+                for (int num = 1; num <= 9; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (solveSudoku(board)) {
+                            return true;
+                        }
+                        board[i][j] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// Function to print the Sudoku board
+void printBoard(const vector<vector<int>>& board) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j] << " ";
+            if ((j + 1) % 3 == 0 && j < 8) {
+                cout << "| ";
+            }
+        }
+        cout << endl;
+        if ((i + 1) % 3 == 0 && i < 8) {
+            cout << "-------------------------" << endl;
+        }
+    }
+}
+
+int main() {
+    vector<vector<int>> board(9, vector<int>(9));
+
+    cout << "Enter the Sudoku board (0 for empty cells):" << endl;
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    cout << "\nOriginal Sudoku Board:" << endl;
+    printBoard(board);
+
+    if (solveSudoku(board)) {
+        cout << "\nSolved Sudoku Board:" << endl;
+        printBoard(board);
+    } else {
+        cout << "\nNo solution exists" << endl;
+    }
+
+    return 0;
+}
